217227bea3fada6dd887e90792b4b781b4267dd0
joincs <codingspark786@gmail.com>
2020-12-16 09:07:01 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "My App",
+      home: new HomePage(),
+      theme: new ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.dark,
+        accentColor: Colors.red,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String mytext = "Hello world";
+  void _changeText() {
+    setState(() {
+      if (mytext.startsWith("H")) {
+        mytext = "Welcome to my app";
+      } else {
+        mytext = "Hello world";
+      }
+    });
+  }
+
+  Widget _bodyWidget() {
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(mytext),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+      ),
+      body: _bodyWidget(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: _changeText,
+      ),
+    );
+  }
+}
